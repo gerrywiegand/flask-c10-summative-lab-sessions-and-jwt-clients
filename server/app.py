9dcfbf055cda9224210388b3161fea06c9f6d2d5
Gerry <gerrywiegand@gmail.com>
2025-11-29 17:35:24 -0500

    placed JWT global guard and GetUser endpoint

--- server/app.py
+++ server/app.py
@@ -1,5 +1,6 @@
 from config import Config
-from flask import Flask, jsonify, make_response, request, session
+from flask import Flask, jsonify, make_response, request, 
+from flask_bcrypt import Bcrypt
 from flask_jwt_extended import (
     JWTManager,
     create_access_token,
@@ -7,7 +8,11 @@ from flask_jwt_extended import (
     verify_jwt_in_request,
 )
 from flask_restful import Api, Resource
-from models import User, bcrypt, db, user_schema
+from flask_sqlalchemy import SQLAlchemy
+from models import User, user_schema
+
+db = SQLAlchemy()
+bcrypt = Bcrypt()
 
 app = Flask(__name__)
 app.config.from_object(Config)
@@ -15,6 +20,14 @@ db.init_app(app)
 bcrypt.init_app(app)
 api = Api(app)
 jwt = JWTManager(app)
+
+open_routes = ["signup", "login"]
+
+
+@app.before_request
+def check_if_logged_in():
+    if request.endpoint not in open_routes:
+        verify_jwt_in_request()
 
 
 class Signup(Resource):
@@ -38,7 +51,7 @@ class Signup(Resource):
         except ValueError as ve:
             return {"message": str(ve)}, 400
 
-        return {"Message:User created successfully"}, user_schema.dump(new_user), 201
+        return {"Message:User created successfully", user_schema.dump(new_user)}, 201
 
 
 class Login(Resource):
@@ -60,18 +73,18 @@ class Login(Resource):
             return {"message": "Invalid username or password."}, 401
 
 
-class get_current_user(Resource):
+class GetUser(Resource):
     def get(self):
-        if verify_jwt_in_request():
-            user_id = get_jwt_identity()
-            user = User.query.get(user_id)
-            return user_schema.dump(user), 200
-        else:
-            return {}, 401
+       get_jwt_identity()
+        user_id = get_jwt_identity()
+        user = User.query.get(user_id)
+        if not user:
+            return {"message": "User not found."}, 404
 
 
 api.add_resource(Signup, "/signup", endpoint="signup")
 api.add_resource(Login, "/login", endpoint="login")
+api.add_resource(GetUser, "/me", endpoint="me")
 
 if __name__ == "__main__":
     with app.app_context():
